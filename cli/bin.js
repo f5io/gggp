5d350440bf898271d996e698c8e668c6ac50629a
Joe Harlow <joe@f5.io>
2016-11-05 18:03:31 +0000

    fix path issue

--- cli/bin.js
+++ cli/bin.js
@@ -1,6 +1,5 @@
 #!/usr/bin/env node
 const pkg = require('../package');
-const path = require('path');
 const program = require('commander');
 const init = require('../lib/init');
 const get = require('../lib/get');
@@ -46,11 +45,10 @@ program
   .option('-s, --sha <sha>', 'sha of the github proto to use')
   .option('-p, --path <path>', 'path to store protos locally')
   .option('-b, --branch <branch>', 'branch of the .proto file in git')
-  .action((proto, { sha, path: pth = 'protos', branch = 'master' }) => {
+  .action((proto, { sha, path, branch = 'master' }) => {
     logo();
-    const protoPath = path.join(process.cwd(), pth);
     return (proto
-      ? get(pth, [ proto, sha, branch ])
+      ? get(path, [ proto, sha, branch ])
       : init())
       .then(({ met, unmet }) => {
         if (unmet.length) process.exit(1);
